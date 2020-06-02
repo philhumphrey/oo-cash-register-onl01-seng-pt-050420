fff0bfeb6c01b90976be78172095df6bd27f9194
Phil Humphrey <phumphrey588@yahoo.com>
2020-06-02 03:24:12 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,80 @@
 
+
+# class CashRegister
+  
+#   attr_accessor :new, :total, :discount, :items
+  
+# def initialize(discount = 0)
+#     @total = 0
+#     @discount = discount
+#     @items = []
+#   end
+  
+#   def add_item(item, price, quantity = 1)
+#     @price = price
+#     @total += price*quantity
+#     if quantity > 1
+#     counter = 0
+#     while counter < quantity
+#     @items << item
+#     quantity += 1
+#   end
+# else
+#   @items << item
+#   end
+# end
+
+#   def apply_discount
+#     if @discount > 0
+#       @to_take_off = (price * discount)/100
+#       @total -= @to_take_off
+#       return "After the discount, the total comes to $#{total}."
+#     else
+#       return "There is no discount to apply."
+#     end
+#   end
+
+#   def void_last_transaction
+#     @total -= @price
+#   end
+# end
+class CashRegister
+    attr_accessor :discount, :cash_register, :total, :price, :items, :last_transaction
+    def initialize(discount = 0)
+    @total = 0
+    @discount = discount
+    @items = []
+    # # @price = price
+    end
+    def add_item(item, price, quantity = 1)
+        @price = price
+        @total += price * quantity
+        if quantity >= 1
+          counter = 0
+          while counter < quantity
+            @items << item
+            counter += 1
+          end
+        else
+          @items << item
+        end
+        self.last_transaction = price * quantity
+      end
+    def apply_discount
+        if @discount > 0
+       update_discount = (price * discount)/100
+       @total -= update_discount
+       return "After the discount, the total comes to $#{total}."
+       else
+        return "There is no discount to apply."
+       end
+    end
+    def void_last_transaction
+    @total -= last_transaction
+    end
+    # def destroy_all(conditions = nil)
+    #     @destroy_all = destroy all
+    #     find(:all, :conditions => conditions).each { |object| object.destroy }
+    # end
+end
+
