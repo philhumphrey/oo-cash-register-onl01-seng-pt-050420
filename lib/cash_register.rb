b524ebb64597d3816f90600b13b0365d85ede8d2
Phil Humphrey <phumphrey588@yahoo.com>
2020-06-02 03:23:20 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -49,7 +49,7 @@ class CashRegister
     def add_item(item, price, quantity = 1)
         @price = price
         @total += price * quantity
-        if quantity > 1
+        if quantity >= 1
           counter = 0
           while counter < quantity
             @items << item
